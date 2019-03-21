e7742857e88143905563065ae858eb9bbff5fc3c
Anton Kurako <goodforgod.dev@gmail.com>
2019-03-21 18:37:28 +0300

    Micro service architecture in progress

---
+++ provider/src/main/java/io/university/provider/controller/OPeopleController.java
@@ -0,0 +1,43 @@
+package io.university.provider.controller;
+
+import io.university.model.dao.oracle.OPerson;
+import io.university.service.factory.impl.OPeopleFactory;
+import io.university.storage.impl.oracle.OPersonStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * ! NO DESCRIPTION !
+ *
+ * @author GoodforGod
+ * @since 01.03.2019
+ */
+@RestController
+@RequestMapping("/oracle")
+public class OPeopleController {
+
+    @Autowired private OPersonStorage peopleStorage;
+    @Autowired private OPeopleFactory factory;
+
+    @GetMapping("/all")
+    public List<OPerson> getAll() {
+        return peopleStorage.findAll();
+    }
+
+    @GetMapping("/fill/{amount}")
+    public List<OPerson> fillWithPeople(@PathVariable(name = "amount", required = false) Integer amount) {
+        final int genAmount = (amount != null && amount > 0)
+                ? amount
+                : ThreadLocalRandom.current().nextInt(2, 4);
+
+        final List<OPerson> people = factory.build(genAmount);
+        peopleStorage.save(people);
+        return people.subList(0, 1);
+    }
+}
---
+++ provider/src/main/java/io/university/provider/service/factory/IFactory.java
@@ -0,0 +1,22 @@
+package io.university.provider.service.factory;
+
+import java.util.List;
+
+/**
+ * ! NO DESCRIPTION !
+ *
+ * @author GoodforGod
+ * @since 01.03.2019
+ */
+public interface IFactory<T> {
+
+    /**
+     * Builds 1 entity
+     */
+    T build();
+
+    /**
+     * Builds N entities
+     */
+    List<T> build(int n);
+}
